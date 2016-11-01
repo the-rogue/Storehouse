fcb2a0c88c28562748ca8063831365458d3c4c91
the-rogue <alexbanwell1@gmail.com>
2016-11-01 22:36:30 +0000

    Added a place to store variables/constants
    Added a place to store variables/constants and moved the appropriate
    variables/constants and updated references.

---
+++ src/main/java/therogue/storehouse/reference/General.java
@@ -0,0 +1,11 @@
+package therogue.storehouse.reference;
+
+public class General {
+	public static final String MODID = "storehouse";
+    public static final String NAME = "Storehouse";
+    public static final String VERSION = "@VERSION@";
+    public static final String MCVERSIONS = "[1.10.2]";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final String SERVER_PROXY_CLASS = "therogue.storehouse.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "therogue.storehouse.proxy.ClientProxy";
+}
--- src/main/java/therogue/storehouse/storehouse.java
+++ src/main/java/therogue/storehouse/storehouse.java
@@ -18,7 +18,7 @@
 package therogue.storehouse;
 
 import therogue.storehouse.proxy.IProxy;
-import net.minecraft.init.Blocks;
+import therogue.storehouse.reference.General;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -27,19 +27,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = storehouse.MODID, name = storehouse.NAME, version = storehouse.VERSION, acceptedMinecraftVersions = storehouse.MCVERSIONS, useMetadata = storehouse.useMetadata)
+@Mod(modid = General.MODID, name = General.NAME, version = General.VERSION, acceptedMinecraftVersions = General.MCVERSIONS, useMetadata = true)
 public class storehouse
 {
-    public static final String MODID = "storehouse";
-    public static final String NAME = "Storehouse";
-    public static final String VERSION = "1.7.2-1.0";
-    public static final String MCVERSIONS = "[1.10.2]";
-    public static final boolean useMetadata = true;
     
     @Instance
     public static storehouse instance;
     
-    @SidedProxy(clientSide = "therogue.storehouse.proxy.ClientProxy", serverSide = "therogue.storehouse.proxy.ServerProxy")
+    @SidedProxy(clientSide = therogue.storehouse.reference.General.CLIENT_PROXY_CLASS, serverSide = therogue.storehouse.reference.General.SERVER_PROXY_CLASS)
     public static IProxy proxy;
     
     @EventHandler
@@ -57,7 +52,6 @@ public class storehouse
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+
     }
 }
