9c4deead155d6edd699b0feccb7d3968f12bd797
the-rogue <alexbanwell1@gmail.com>
2016-11-04 19:27:31 +0000

    Updated Version Scheme
    Added a new version scheme

--- src/main/java/therogue/storehouse/reference/General.java
+++ src/main/java/therogue/storehouse/reference/General.java
@@ -14,7 +14,7 @@ public class General
 {
 	public static final String MODID = "storehouse";
 	public static final String NAME = "Storehouse";
-	public static final String VERSION = "1.10.1-1.0";
+	public static final String VERSION = "1.10.1-0.1.0";
 	public static final String MCVERSIONS = "[1.10.2]";
 	public static final String FINGERPRINT = "";
 	public static final String SERVER_PROXY_CLASS = "therogue.storehouse.proxy.ServerProxy";
