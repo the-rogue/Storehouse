0054e650408250a387e3451d03e248d6ff7d88e0
the-rogue <alexbanwell1@gmail.com>
2016-11-08 18:58:40 +0000

    Fixes and Testing
    Fixed Transparency glitch with azurite dust texture
    Testing a possible fix for Update.json not working

--- src/main/java/therogue/storehouse/storehouse.java
+++ src/main/java/therogue/storehouse/storehouse.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 
-@Mod(modid = General.MOD_ID, name = General.MOD_NAME, version = General.VERSION, acceptedMinecraftVersions = General.MC_VERSIONS, useMetadata = true, guiFactory = General.GUI_FACTORY, updateJSON = "https://raw.githubusercontent.com/the-rogue/Storehouse-Expansion/master/Misc%20Files/update.json")
+@Mod(modid = General.MOD_ID, name = General.MOD_NAME, version = General.VERSION, acceptedMinecraftVersions = General.MC_VERSIONS, useMetadata = true, guiFactory = General.GUI_FACTORY, updateJSON = "https://raw.githubusercontent.com/the-rogue/Storehouse-Expansion/master/Misc_Files/update.json")
 public class storehouse
 {
 	@Instance
